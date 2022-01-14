5547c405806e88c37a592e6bf66609b7991a1b1b
Jon Cram <webignition@gmail.com>
2022-01-14 18:50:22 +0000

    Use phpstan level 9

--- tests/Unit/IdentifierTypeAnalyserTest.php
+++ tests/Unit/IdentifierTypeAnalyserTest.php
@@ -132,7 +132,7 @@ class IdentifierTypeAnalyserTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @return array[]
+     * @return array<mixed>
      */
     public function cssSelectorDataProvider(): array
     {
@@ -177,7 +177,7 @@ class IdentifierTypeAnalyserTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @return array[]
+     * @return array<mixed>
      */
     public function xPathExpressionDataProvider(): array
     {
@@ -213,7 +213,7 @@ class IdentifierTypeAnalyserTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @return array[]
+     * @return array<mixed>
      */
     public function attributeSelectorDataProvider(): array
     {
@@ -264,7 +264,7 @@ class IdentifierTypeAnalyserTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @return array[]
+     * @return array<mixed>
      */
     public function unknownTypeDataProvider(): array
     {
@@ -291,7 +291,7 @@ class IdentifierTypeAnalyserTest extends \PHPUnit\Framework\TestCase
     }
 
     /**
-     * @return array[]
+     * @return array<mixed>
      */
     public function descendantDomIdentifierDataProvider(): array
     {
